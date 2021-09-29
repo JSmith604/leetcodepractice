47897688cc169ff8f2117fb7ab41969fa89ca679
JSmith604 <jacquelinesmith190@gmail.com>
2021-09-29 23:39:38 +0000

    update

--- twoSumBruteForce.js
+++ twoSumBruteForce.js
@@ -2,7 +2,16 @@ const numbers = [2,7,11,15,21]
 const target = 9
 
 const twoSumBruteForce = (numbers, target) => {
-
+  for(let i = 0; i < numbers.length; i++) {
+    for(let j = 0; j < numbers.length; j++) {
+      if(numbers[i] + numbers[j] === target && numbers[i] !== numbers[j]) {
+        return [i,j]
+      }
+      else {
+        return [];
+      }
+    }
+  }
 };
 
 twoSumBruteForce(numbers, target);
